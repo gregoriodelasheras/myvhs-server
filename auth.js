165a6793fc97192d5e656e343431a660082b71f3
Francisco Gregorio de las Heras <francisco.gregoriodelasheras@gmail.com>
2021-04-15 23:16:19 +0200

    feat: create a new login endpoint + authentication
    - Create a new login endpoint for registered users that contains logic for authenticating users with basic HTTP authentication and generating a JWT token for authenticating future requests.

---
+++ auth.js
@@ -0,0 +1,33 @@
+const jwtSecret = 'your_jwt_secret';
+
+const jwt = require('jsonwebtoken');
+const passport = require('passport');
+
+require('./passport');
+
+const generateJWTToken = (user) => jwt.sign(user, jwtSecret, {
+  subject: user.username,
+  expiresIn: '7d',
+  algorithm: 'HS256',
+});
+
+module.exports = (router) => {
+  router.post('/login', (req, res) => {
+    // eslint-disable-next-line consistent-return
+    passport.authenticate('local', { session: false }, (err, user) => {
+      if (err || !user) {
+        return res.status(400).json({
+          message: 'Oops! Something went wrong. Please try again.',
+          user,
+        });
+      }
+      req.login(user, { session: false }, () => {
+        if (err) {
+          res.send(err);
+        }
+        const token = generateJWTToken(user.toJSON());
+        return res.json({ user, token });
+      });
+    })(req, res);
+  });
+};
