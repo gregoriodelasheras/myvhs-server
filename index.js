cb5ae7cee678d567529af0b31812b0d232b51c6b
Francisco Gregorio de las Heras <francisco.gregoriodelasheras@gmail.com>
2021-04-20 21:23:06 +0200

    refactor: change start script and listening port

--- index.js
+++ index.js
@@ -455,6 +455,7 @@ app.use(cors({
 }));
 
 // Listen for requests
-app.listen(8080, () => {
-  console.log('Your app is listening on port 8080.');
+const port = process.env.PORT || 8080;
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Listening on Port ${port}`);
 });
